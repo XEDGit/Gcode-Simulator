f40781469f751da6851c18cad82914347b8efb92
XEDGit <muzio200@gmail.com>
2021-08-02 03:15:48 +0200

    v1.1 COLORS UPDATE & minor bug fixes

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# Gcode-Simulator
-A C language G-code (file for 3d printing) interpreter and simulator, really useless but not a bad excercise for file and string handling. It's my first C project so if anyone has suggestions I'll be happy to read them :)
+# Gcode-Simulator v1.1
+A C language G-code (file extension for 3d printing) interpreter and simulator, really useless but not a bad excercise for file and string handling. It's my first C project so if anyone has suggestions I'll be happy to read them :)
 
 There are example files in 'Gcode Simulator Files' folder, but you can use the file you want!
 
-Included Options:
+Included Features:
   - Frame-by-frame view
   - Pause
-  - Simulation speed manager
+  - Simulation speed manager (v1.1)
   - Clear filament trace
   - Live printer settings reading
+  - UI with colors (v1.1)
--- builds/gcodeSimulator.c
+++ builds/gcodeSimulator.c
@@ -7,296 +7,315 @@ int fbfMode = 0;
 int menuIndex = 0;
 int indexBkp = 0;
 int subMenu = 0;
+
 //file finder
 DIR *directory;
 struct dirent *dirContent;
 int fileNumber;
 char fileList[100][50];
+
 //simulator
 char brokenCommand[10][40];
 char simulatedLine[59] = "|                                                       |\n";
-typedef struct {
-    int x, y, z, speed, feedRate; 
-    float pFeedRate;
+typedef struct
+{
+	int x, y, z, speed, feedRate;
+	float pFeedRate;
 } Agent;
 Agent agente = {0};
 int moved;
-
-typedef struct {
-    int x, y;
+typedef struct
+{
+	int x, y;
 } History;
 History tail[20000];
-
 int lastHRounds;
-
 unsigned int rounds = 0;
-
 struct stat checkFolder = {0};
-
 int speed = 1;
 
-void clr() {
-    system("cls");
+//utility
+void clr()
+{
+	system("cls");
 }
 
-void clearTail() {
-    int count = 0;
-        while (count <= lastHRounds)
-        {
-            tail[count].x = '\0';
-            tail[count].y = '\0';
-            count++;
-        }
+//clear filament option
+void clearTail()
+{
+	int count = 0;
+	while (count <= lastHRounds)
+	{
+		tail[count].x = '\0';
+		tail[count].y = '\0';
+		count++;
+	}
 }
 
-void printResult(char res[29][59]) {
+//print res[][] array containing rendered frame
+void printResult(char res[29][59])
+{
 
-    clr();
-    int tttt = 2;
+	clr();
+	int tttt = 2;
 
-    printf("_________________________________________________________\n\n");
-    printf("\t\t  Gcode Simulator\n\n\n");
-    printf("\t\t\tSimulator\n\n");
-    printf(" _______________________________________________________\n");
+	printf("_________________________________________________________\n\n");
+	printf("\t\t\033[0;34mGcode Simulator\033[0;37m\n\n\n");
+	printf("\t\t  \033[0;36mSimulator\033[0;37m\n\n");
+	printf(" _______________________________________________________\n");
 
-    while (tttt < 28)
-    {
-        printf("%s", res[tttt]);
-        tttt++;
-    }
+	while (tttt < 28)
+	{
+		printf("%s", res[tttt]);
+		tttt++;
+	}
 
-    printf(" _______________________________________________________\n");
-    
-    printf("\n\n\tPrinter speed: %d", agente.speed);
-	printf("\n\n\tSimulation speed: x%d", speed);
-    printf("\n\tFeed Rate: %d", agente.feedRate);
-    printf("\n\tX: %d\tY: %d\tZ: %d", agente.x, agente.y, agente.z);
-    if (fbfMode == 1)
-    {
-        printf("\n\n\tPress any key to see next frame, press X to exit");
-    } else  {
-        printf("\n\tOptions\n\tV: Pause\tC: Clear filament trace\n\tX: Toggle frame-by-frame mode\n\tN: Decrease Sim. speed\tM: Increase Sim. speed\n\tZ: Exit");
-    }
+	printf(" _______________________________________________________\n");
+
+	printf("\n\n\t\033[0;33mSimulation speed: x%d", speed);
+	printf("\n\n\tPrinter speed: %d", agente.speed);
+	printf("\n\tFeed Rate: %d\033[0;37m", agente.feedRate);
+	printf("\n\t\033[0;32mX: %d\tY: %d\tZ: %d\033[0;37m", agente.x, agente.y, agente.z);
+	if (fbfMode == 1)
+	{
+		printf("\n\n\tPress \033[0;33many key\033[0;37m to see next frame, \033[0;31mX\033[0;37m to exit");
+	}
+	else
+	{
+		printf("\n\n\t\033[0;33mOPTIONS\n\tV: Pause\tC: Clear filament trace\n\tX: Toggle frame-by-frame mode\033[0;37m\n\t\033[0;35mN: Decrease Sim. speed\tM: Increase Sim. speed\033[0;37m\n\t\033[0;31mZ: Exit\033[0;37m");
+	}
 }
 
-int printSimulation(char line[], int *index) {
+//interpretate string command and render frame and trace based on it
+int printSimulation(char line[], int *index)
+{
 
-    //break down line in single command into brokencommand[]
+	//break down line in single command into brokencommand[]
+	unsigned int counter = 0;
+	int commandCounter = 0;
+	int commandSubCounter = 0;
+	int skipDecimals = 0;
+	char result[29][59];
 
-    unsigned int counter = 0;
-    int commandCounter = 0;
-    int commandSubCounter = 0;
-    int skipDecimals = 0;
-    char result[29][59];
+	if (line[0] == ';')
+	{
+		return (0);
+	}
 
-    if (line[0] == ';') {
-        return 0;
-    }
+	while (strlen(line) > counter)
+	{
+		if (line[counter] == '.')
+		{
+			skipDecimals = 1;
+		}
 
-    while (strlen(line) > counter)
-    {
-        if (line[counter] == '.')
-        {
-            skipDecimals = 1;
-        }
-        
+		if (line[counter] == ' ')
+		{
+			commandCounter++;
+			commandSubCounter = 0;
+			skipDecimals = 0;
+		}
+		else if (skipDecimals == 0)
+		{
+			brokenCommand[commandCounter][commandSubCounter] = line[counter];
+			commandSubCounter++;
+		}
+		else
+		{
+			brokenCommand[commandCounter][commandSubCounter] = '\0';
+		}
+		counter++;
+	}
+	rounds++;
 
-        if (line[counter] == ' ')
-        {
-            commandCounter++;
-            commandSubCounter = 0;
-            skipDecimals = 0;
-        } else if (skipDecimals == 0)  {
-            brokenCommand[commandCounter][commandSubCounter] = line[counter];
-            commandSubCounter++;
-        } else {
-            brokenCommand[commandCounter][commandSubCounter] = '\0';
-        }
-        counter++;
-    }
+	//read and execute single commands
+	int brokenCounter = 0;
+	while (brokenCounter <= commandCounter)
+	{
+		if (brokenCommand[brokenCounter][0] == ';')
+		{
+			break;
+		}
 
-    rounds++;
+		char digits[5];
+		strcpy(digits, brokenCommand[brokenCounter]);
+		memmove(&digits[0], &digits[1], strlen(digits));
+		int number;
+		number = atoi(digits);
 
-    //read and execute single commands
-    
-    int brokenCounter = 0;
-    while (brokenCounter <= commandCounter)
-    {
-        if (brokenCommand[brokenCounter][0] == ';')
-        {
-            break;
-        }
-        
-        char digits[5];
-        strcpy(digits, brokenCommand[brokenCounter]);
-        memmove(&digits[0], &digits[1], strlen(digits));
-        int number;
-        number = atoi(digits);
+		switch (brokenCommand[brokenCounter][0])
+		{
+		case 'F':
+			agente.feedRate = number;
+			break;
+		case 'S':
+			agente.speed = number;
+			break;
+		case 'X':
+			if ((int)agente.x / 4 != (int)number / 4)
+			{
+				moved = 1;
+			}
+			agente.x = number;
+			break;
+		case 'Y':
+			if ((int)agente.y / 8 != (int)number / 8)
+			{
+				moved = 1;
+			}
+			agente.y = number;
+			break;
+		case 'Z':
+			agente.z = number;
+			break;
+		default:
+			break;
+		}
+		brokenCounter++;
+	}
 
-        switch (brokenCommand[brokenCounter][0])
-        {
-            case 'F':
-                agente.feedRate = number;
-                break;
-            case 'S':
-                agente.speed = number;
-                break;
-            case 'X':
-                if ((int)agente.x/4 != (int)number/4)
-                {
-                    moved = 1;
-                }
-                agente.x = number;
-                break;
-            case 'Y':
-                if ((int)agente.y/8 != (int)number/8)
-                {
-                    moved = 1;
-                }
-                agente.y = number;
-                break;
-            case 'Z':
-                agente.z = number;
-                break;
-            default:
-                break;
-        }
-        brokenCounter++;
-    }
-    //print result
+	//print result
+	if (moved == 0)
+	{
+		return (1);
+	}
 
-    if (moved == 0)
-    {
-        return 1;
-    }
+	int lineCounter = 0;
+	while (lineCounter < 28)
+	{
+		char modifiedLine[59];
+		strcpy(modifiedLine, simulatedLine);
 
-    int lineCounter = 0;
-    while (lineCounter < 28)
-    {
-        char modifiedLine[59];
-        strcpy(modifiedLine, simulatedLine);
+		unsigned int tailCounter = 0;
+		while (tailCounter < (sizeof(tail) / sizeof(tail[0])) - 2 && tailCounter != rounds)
+		{
+			if (tail[tailCounter].y / 8 == lineCounter)
+			{
+				modifiedLine[(tail[tailCounter].x / 4) + 1] = '-';
+			}
+			tailCounter++;
+		}
 
-        unsigned int tailCounter = 0;
-        while (tailCounter < (sizeof(tail)/sizeof(tail[0]))-2 && tailCounter != rounds)
-        {
-            if (tail[tailCounter].y/8 == lineCounter)
-            {
-                modifiedLine[(tail[tailCounter].x/4)+1] = '-';
-            }
-            tailCounter++;
-        }
-        
-        
-        if (agente.y/8 != lineCounter)
-        {
-            strcpy(result[lineCounter], modifiedLine);
-            
-        } else {
-            modifiedLine[(agente.x/4)+1] = 'O';
-            strcpy(result[lineCounter], modifiedLine);
-        }
-        lineCounter++;
-    }
+		if (agente.y / 8 != lineCounter)
+		{
+			strcpy(result[lineCounter], modifiedLine);
+		}
+		else
+		{
+			modifiedLine[(agente.x / 4) + 1] = 'O';
+			strcpy(result[lineCounter], modifiedLine);
+		}
+		lineCounter++;
+	}
 
-    if (fbfMode == 1)
-    {
-        char x = getch();
-        if (x == 'x') {
-            fbfMode = 0;
-        }
-    }
-    
-    if (*index >= speed)
+	//frame by frame mode
+	if (fbfMode == 1)
+	{
+		char x = getch();
+		if (x == 'x')
+		{
+			fbfMode = 0;
+		}
+	}
+
+	//speed option
+	if (*index >= speed)
 	{
 		*index = 1;
 	}
 	else
 	{
 		*index = *index + 1;
-		return 1;
+		return (1);
 	}
 	if (speed < 1)
 	{
 		speed = 1;
 	}
 
-    printResult(result);
-    
-    return 1;
+	//printing funtion
+	printResult(result);
 
+	return (1);
 }
 
-void historySimulation() {
+void historySimulation()
+{
 
-    int contTemp = 0;
-    int roundTemp = 0;
-    while (tail[roundTemp].x != '\0' && tail[roundTemp].x != '\0')
-    {
-        if (agente.x/4 == tail[contTemp].x/4 && agente.y/8 == tail[contTemp].y/8)
-        {
-            roundTemp++;
-            continue;
-        }
-        
-        roundTemp++;
-        contTemp++;
-    }
-    lastHRounds = roundTemp;
-    History temp = {0,0};
-    temp.x = agente.x;
-    temp.y = agente.y;
-    tail[contTemp] = temp;
+	int contTemp = 0;
+	int roundTemp = 0;
+	while (tail[roundTemp].x != '\0' && tail[roundTemp].x != '\0')
+	{
+		if (agente.x / 4 == tail[contTemp].x / 4 && agente.y / 8 == tail[contTemp].y / 8)
+		{
+			roundTemp++;
+			continue;
+		}
+
+		roundTemp++;
+		contTemp++;
+	}
+	lastHRounds = roundTemp;
+	History temp = {0, 0};
+	temp.x = agente.x;
+	temp.y = agente.y;
+	tail[contTemp] = temp;
 }
 
-void escape() {
-    menuIndex = 0;
-    subMenu = 0;
+void escape()
+{
+	menuIndex = 0;
+	subMenu = 0;
 	speed = 1;
-    }
+}
 
-void simulate() {
-    char fullPath[50] = "./Gcode Simulator Files/";
-    strcat(fullPath, fileList[fileNumber]);
-    FILE *file = fopen(fullPath, "r");
-    char line[256];
+void simulate()
+{
+	char fullPath[50] = "./Gcode Simulator Files/";
+	strcat(fullPath, fileList[fileNumber]);
+	FILE *file = fopen(fullPath, "r");
+	char line[256];
 	int index = 1;
-    if (file != NULL)
-    {
-        while (fgets(line, sizeof(line), file))
-        {
-            if (printSimulation(line, &index) == 1)
-            {
-                historySimulation();
-            }
-            if (kbhit() != 0 && fbfMode == 0)
-            {
-                char key = getch();
-                if (key == 'z')
-                {
-                    printf("\n\tSimulation Terminated, press Enter to get back to Main Menu...\n");
-                    escape();
-                    break;
-                }
-                else if (key == 'x')
-                {
-                    if (fbfMode == 0)
-                    {
-                        fbfMode++;
-                        clr();
-                        printf("\n\n\t\tPress any key to see next frame, X to exit");
-                    } else {
-                        fbfMode--;
-                    }
-                }
-                else if (key == 'v')
-                {
-                    printf("\n\n\tPress Enter to restart...");
-                    getchar();
-                }
-                else if (key == 'c')
-                {
-                    clearTail();
-                }
+	if (file != NULL)
+	{
+		while (fgets(line, sizeof(line), file))
+		{
+			if (printSimulation(line, &index) == 1)
+			{
+				historySimulation();
+			}
+			if (kbhit() != 0 && fbfMode == 0)
+			{
+				char key = getch();
+				if (key == 'z')
+				{
+					printf("\n\tSimulation Terminated, press Enter to get back to Main Menu...\n");
+					escape();
+					break;
+				}
+				else if (key == 'x')
+				{
+					if (fbfMode == 0)
+					{
+						speed = 1;
+						fbfMode++;
+						clr();
+						printf("\n\n\t\tPress \033[0;33many key\033[0;37m to see next frame, \033[0;31mX\033[0;37m to exit");
+					}
+					else
+					{
+						fbfMode--;
+					}
+				}
+				else if (key == 'v')
+				{
+					printf("\n\n\tPress \033[0;33mEnter\033[0;37m to restart...");
+					getchar();
+				}
+				else if (key == 'c')
+				{
+					clearTail();
+				}
 				else if (key == 'm')
 				{
 					speed++;
@@ -305,121 +324,123 @@ void simulate() {
 				{
 					speed--;
 				}
-            }
-        }
-        clearTail();
-        agente.x = 0;
-        agente.y = 0;
-        agente.z = 0;
-        rounds = 0;
-        fclose(file);
-        getchar();
-    }
+			}
+		}
+		clearTail();
+		agente.x = 0;
+		agente.y = 0;
+		agente.z = 0;
+		rounds = 0;
+		fclose(file);
+		getchar();
+	}
 }
 
-void printMenu() {
-    clr();
-    switch (menuIndex)
-    {
+void printMenu()
+{
+	clr();
+	switch (menuIndex)
+	{
 
-    //main menu
-    case 0:
-        printf("_________________________________________________________\n\n");
-        printf("Welcome to Gcode Simulator written in C by XEDGit\n\n");
-        printf("\tChoose an option: (1: Simulate |2: Exit)\n");
-        scanf("%d", &menuIndex);
-        
-        while (menuIndex > 2 || menuIndex < 0)
-        {
-            printf("Choose a valid option: \n");
-            scanf("%d", &menuIndex);
-        }
-        
-        break;
+	//main menu
+	case 0:
+		printf("_________________________________________________________\n\n");
+		printf("\tWelcome to \033[0;34mGcode Simulator\033[0;37m written in \033[0;32mC\033[0;37m \n\t\tby \033[0;36mXEDGit\033[0;37m\n\n");
+		printf("\tChoose an option: (1: \033[0;32mSimulate\033[0;37m |2: \033[0;31mExit\033[0;37m)\n");
+		scanf("%d", &menuIndex);
 
-    //simulator
-    case 1:
-        switch (subMenu)
-        {
-            //choose file
-        case 0:
-            printf("_________________________________________________________\n\n");
-            printf("\t\t  Gcode Simulator\n\n\n");
-            printf("\t\t\tSimulator\n\n");
+		while (menuIndex > 2 || menuIndex < 0)
+		{
+			menuIndex = 0;
+			printf("\033[0;31mChoose a valid option:\033[0;37m \n");
+			scanf("%d", &menuIndex);
+		}
+		getchar();
+		break;
 
-            directory = opendir("./Gcode Simulator Files");
-            int label = 0;
+	//simulator
+	case 1:
+		switch (subMenu)
+		{
+			//choose file
+		case 0:
+			printf("_________________________________________________________\n\n");
+			printf("\t\t\033[0;34mGcode Simulator\033[0;37m\n\n\n");
+			printf("\t\t  \033[0;36mSimulator\033[0;37m\n\n");
 
-            while ((dirContent = readdir(directory)) != NULL)
-            {
-                printf("\t%d\t", label);
-                strcpy(fileList[label], dirContent->d_name);
-                printf("%s\n", dirContent->d_name);
-                label += 1;
-            }
-            printf("\t%d\tBack\n\n", label);
-        
-            closedir(directory);
-            printf("\tChoose file number:\n");
-            scanf("\t%d", &fileNumber);
-            while (fileNumber > label || fileNumber < 2)
-            {
-                printf("\tError. Insert valid file number:\n");
-                scanf("\t%d", &fileNumber);
-            }
+			directory = opendir("./Gcode Simulator Files");
+			int label = 0;
 
-            if (fileNumber == label)
-            {
-                escape();
-                label = 0;
-                return;
-            }
-            
-            
-            subMenu = 1;
-            label = 0;
-            break;
+			while ((dirContent = readdir(directory)) != NULL)
+			{
+				printf("\t%d\t", label);
+				strcpy(fileList[label], dirContent->d_name);
+				printf("%s\n", dirContent->d_name);
+				label += 1;
+			}
+			printf("\t%d\tBack\n\n", label);
 
-            //read and simulate selected file
-            case 1:
-                printf("_________________________________________________________\n\n");
-				printf("\t\t  Gcode Simulator\n\n\n");
-				printf("\t\t\tSimulator\n\n");
-
-				simulate();
+			closedir(directory);
+			printf("\tChoose file number:\n");
+			scanf("\t%d", &fileNumber);
+			while (fileNumber > label || fileNumber < 2)
+			{
+				printf("\t\033[0;31mError. Insert valid file number:\033[0;37m\n");
+				scanf("\t%d", &fileNumber);
 				getchar();
+			}
+
+			if (fileNumber == label)
+			{
 				escape();
+				label = 0;
+				return;
+			}
 
-                break;
-        }
-        
-        break;
+			subMenu = 1;
+			label = 0;
+			break;
 
-    case 2: 
+		//read and simulate selected file
+		case 1:
+		
+			simulate();
+			getchar();
+			escape();
 
-        exit(0);
-    }
-    return;
+			break;
+		}
+
+		break;
+
+	case 2:
+
+		exit(0);
+	}
+	return;
 }
 
-int main() {
-    
-    agente.speed = 100;
-    agente.x = 0;
-    agente.y = 0;
+int main()
+{
+	//check souce folder existance and create if not
+	agente.speed = 100;
+	agente.x = 0;
+	agente.y = 0;
 
-    if (stat("./Gcode Simulator Files", &checkFolder) == -1) {
-        mkdir("./Gcode Simulator Files");
-    }
+	if (stat("./Gcode Simulator Files", &checkFolder) == -1)
+	{
+		mkdir("./Gcode Simulator Files");
+	}
 
-    while (menuIndex != 5)
-    {
-        if (menuIndex > 3 || menuIndex < 0)
-        {
-            escape();
-        }
-        
-        printMenu();
-    }
-    return(0);
+	//error handling
+	while (menuIndex != 5)
+	{
+		if (menuIndex > 3 || menuIndex < 0)
+		{
+			escape();
+		}
+
+		printMenu();
+	}
+	return (0);
 }
